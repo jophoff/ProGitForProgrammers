b9d7d9a2b8a96c6978f3166b5428663b3c8cc491
Jacques Ophoff <j.ophoff@abertay.ac.uk>
2022-10-05 10:28:57 +0100

    Update made using GitHub Desktop

--- ProGitForProgrammers/ProGitForProgrammers/Program.cs
+++ ProGitForProgrammers/ProGitForProgrammers/Program.cs
@@ -9,7 +9,7 @@ namespace ProGitForProgrammers
             Console.WriteLine("Hello World!");
             Console.WriteLine("New line of code");
 	        Console.WriteLine("A final line of code is added");
-	        Console.WriteLine("Change made in Visual Studio");
+	        Console.WriteLine("Change made in GitHub Desktop");
         }
     }
 }
