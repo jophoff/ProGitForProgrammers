a685413311b827fd1c3ea3b9a2d7d85b7c56878a
Jacques Ophoff <j.ophoff@abertay.ac.uk>
2022-10-06 12:27:39 +0100

    My code updated

--- ProGitForProgrammers/ProGitForProgrammers/Program.cs
+++ ProGitForProgrammers/ProGitForProgrammers/Program.cs
@@ -7,7 +7,7 @@ namespace ProGitForProgrammers
         static void Main(string[] args)
         {
             Console.WriteLine("Important line of code");
-            Console.WriteLine("New line of code");
+            Console.WriteLine(">> my code");
 	        Console.WriteLine("A final line of code is added");
 	        Console.WriteLine("Change made in GitHub Desktop");
         }
